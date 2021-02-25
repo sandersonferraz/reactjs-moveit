32b1bd239a485fe839d3056398b38ac081c4cb5c
Sanderson Ferraz <sandersonferraz@gmail.com>
2021-02-25 18:38:53 -0300

    Added to images on diretory public. Created components ExperienceBar and Profile, created CSS module ExperienceBar, Home and Profile, modified CSS module global.

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,12 +1,14 @@
+import styles from "../styles/components/ExperienceBar.module.css"; // Importando css module para o scopo do ExperienceBar.
+
 export function ExperienceBar() {
     return (
-        <header className="experience-bar">
+        <header className={styles.experienceBar}>
             <span>0 px</span>
             <div>
                 <div style={{ width: "60%" }} />
-                <span className="current-experience" style={{ left: "60%" }}>300 px</span>
+                <span className={styles.currentExperience} style={{ left: "60%" }}>300 px</span>
             </div>
             <span>600 px</span>
         </header>
     );
-} 
+}
